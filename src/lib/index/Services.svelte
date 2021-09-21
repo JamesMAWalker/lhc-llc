f0e1a6b7b1e2b39c7598d080bee4e4398d454b83
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2021-09-21 16:32:31 +0700

    Services: refactored to separate component.

---
+++ src/lib/index/Services.svelte
@@ -0,0 +1,18 @@
+<div class="landing">
+	<section class="services">SERVICES</section>
+</div>
+
+<style lang="scss">
+	.landing {
+		background-color: transparent;
+	}
+	.services {
+		height: 100vh;
+		width: 100vw;
+		background: var(--white);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		color: var(--white);
+	}
+</style>
