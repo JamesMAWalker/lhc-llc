76a82ae08b84dc8e8c54a4c8d6fbcac1dec1ca4c
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2021-09-24 21:54:27 +0700

    Add more font weights.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,23 +3,22 @@
 </script>
 
 <script>
-import Hero from '$lib/index/Hero.svelte'
-import Services from '$lib/index/Services.svelte'
-
+	import Hero from '$lib/index/Hero.svelte';
+	import Services from '$lib/index/Services.svelte';
 </script>
 
 <svelte:head>
 	<title>Home</title>
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect"	 href="https://fonts.gstatic.com" crossorigin />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 	<link
-		href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400&display=swap"
+		href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;500;600;700;800&display=swap"
 		rel="stylesheet"
 	/>
 </svelte:head>
 
 <div class="landing">
-	<Hero/>
+	<Hero />
 	<Services />
 </div>
 
