0d0be5f5c899d4d6c443d4d2bae9911cd46b6d98
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2021-09-24 21:50:44 +0700

    Refactor title block into SectionHeader component.

---
+++ src/lib/SectionHeader.svelte
@@ -0,0 +1,40 @@
+<script>
+	let sub
+	let titleBlue
+	let titleYellow
+</script>
+
+<div class="title-block">
+	<h3 class="sub">{sub}</h3>
+	<h2 class="title">
+		<span>{titleBlue}</span>
+		<span>{titleYellow}</span>
+	</h2>
+</div>
+
+<style lang="scss">
+	.title-block {
+		color: var(--text-color);
+		.sub {
+			padding: 0;
+			margin: 0;
+			margin-bottom: var(--space-md);
+			font-size: var(--text-md);
+			font-weight: 200;
+			text-transform: uppercase;
+		}
+		.title {
+			padding: 0;
+			margin: 0;
+			display: flex;
+			flex-direction: column;
+			align-items: flex-start;
+			justify-content: flex-start;
+			font-size: var(--text-header);
+			font-weight: 600;
+			span:last-of-type {
+				color: var(--accent-color);
+			}
+		}
+	}
+</style>
