70e40faca7347db31ab15f02ad2d96ca7ebec7b4
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2021-09-24 21:50:14 +0700

    Create 60% width container component.

---
+++ src/lib/containers/ContainerSixty.svelte
@@ -0,0 +1,18 @@
+<script>
+	// your script goes here
+</script>
+
+<div class="container-60">
+  <slot/>
+</div>
+
+<style>
+	.container-60 {
+		width: 60vw;
+		overflow: visible;
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		justify-content: flex-start;
+	}
+</style>
