2fc9134c0857c5729c85a71c12422f232e705194
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2021-10-01 23:24:50 +0700

    Move ring and disc buttons to separate components.

---
+++ src/lib/buttons/disc-button.svelte
@@ -0,0 +1,40 @@
+<script>
+</script>
+
+<button class="disc-button">
+	<span class="disc" />
+	learn more
+</button>
+
+<style lang="scss">
+	.disc-button {
+		cursor: pointer;
+		position: relative;
+		margin-top: 1rem;
+		padding-left: 1rem;
+		font-size: var(--text-xl);
+		font-weight: 700;
+		color: var(--white);
+		background-color: transparent;
+		border: none;
+		&:hover {
+			.disc {
+				transform: translate(-5%, -45%) scale(1.1);
+			}
+		}
+		.disc {
+			z-index: var(--base-one);
+			position: absolute;
+			top: 50%;
+			left: 0;
+			transform: translate(-5%, -45%);
+			height: var(--circle-med);
+			width: var(--circle-med);
+			height: 40px;
+			width: 40px;
+			background-color: var(--accent-tr1);
+			border-radius: var(--radius-rounded);
+			transition: var(--transition-3-smooth);
+		}
+	}
+</style>
---
+++ src/lib/buttons/ring-button.svelte
@@ -0,0 +1,37 @@
+<script>
+	import { goto } from '$app/navigation';
+</script>
+
+<button on:click={() => goto('/#services')} class="ring-btn">
+	<span class="ring" />
+	<span class="text">services</span>
+	<span class="down-arr">↓</span>
+</button>
+
+<style lang="scss">
+	.ring-btn {
+		isolation: isolate;
+		cursor: pointer;
+		position: relative;
+		margin-top: var(--space-lg);
+		background-color: transparent;
+		border: none;
+		color: var(--text-color);
+		font-size: var(--text-prose);
+		font-weight: var(--semibold);
+		.down-arr {
+			color: var(--accent-color);
+		}
+		.ring {
+			z-index: var(--base-one);
+			position: absolute;
+			top: 50%;
+			left: -5%;
+			transform: translateY(-50%);
+			height: 30px;
+			width: 30px;
+			border-radius: var(--radius-rounded);
+			border: 1px solid var(--accent-color);
+		}
+	}
+</style>
