3d3f7567e3574faa39e7ff8ae13d412814515412
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2021-10-04 17:26:07 +0700

    Add contact pagecomponent.

---
+++ src/routes/contact.svelte
@@ -0,0 +1,18 @@
+<script>
+	// your script goes here
+</script>
+
+<!-- markup (zero or more items) goes here -->
+<section class="contact-page">
+  Contact Page
+</section>
+
+<style>
+  .contact-page {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
