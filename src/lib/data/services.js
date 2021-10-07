d4aba60cc10bd449aa4a48577a9ee4e73c1a81d0
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2021-10-07 21:18:46 +0700

    Create data object for services. Will create API next.

---
+++ src/lib/data/services.js
@@ -0,0 +1,382 @@
+export const services = {
+	'data-facilities': {
+		title: 'Data Facilities',
+		heroImg: '/services/server-blade.jpg',
+		heroHeader: {
+			sub: 'SERVICES',
+			mainColor: 'server',
+			accentColor: 'solutions'
+		},
+		heroBlurb: `One of the most important pieces of equipment in any company’s data center will be its server racks, and these can come in a variety of different models and designs. You might use enclosed cabinets or open frame racks, and they can be two-post or four-post racks, but all of them serve to store the different kinds of equipment which are necessary to your network processing, and your computing capability.`,
+		infoHeader: {
+			sub: 'HOW WE WORK',
+			mainColor: 'our',
+			accentColor: 'process'
+		},
+		infoBlurb: `Whether you're setting up a new datacenter or refitting an existing facility, you don't want to leave your company's data at risk. Our decades of experience with all manner of server form factors and ancillary technologies means that your server facility will be built with security, easy maintenance, and optimal functionality in mind. <br><br> We also offer technical consultation for businesses without deep knowledge of server technologies.`,
+		processList: [
+			{
+				title: 'technical consultation',
+				steps: ['Dolor sit amet', 'Adipiscing elit', 'Mauris eu risus', 'Dapibus neque']
+			},
+			{
+				title: 'patching services',
+				steps: ['Dolor sit amet', 'Adipiscing elit', 'Mauris eu risus', 'Dapibus neque']
+			},
+			{
+				title: 'network setup',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			},
+			{
+				title: 'server racking',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			},
+			{
+				title: 'server blades',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			},
+			{
+				title: 'system maintenance',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			}
+		],
+		contactHeader: {
+			sub: 'CONTACT',
+			mainColor: 'start your <br> networking project',
+			accentColor: 'today'
+		},
+		contactBlurb: `We are there every step of the way for your mission critical systems. From design to installation to network management, your IT infrastructure remains secure and constant. <br><br> Start your network architect services today in order to plan and evolve with the ever-changing technology advantages.`
+	},
+	'fiber': {
+		title: 'Internet & VoIP',
+		heroImg: '/services/fiber.jpg',
+		heroHeader: {
+			sub: 'SERVICES',
+			mainColor: 'fiber optic',
+			accentColor: 'solutions'
+		},
+		heroBlurb: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto a sunt ad tenetur incidunt culpa nisi totam, atque mollitia soluta asperiores veniam, reprehenderit iste tempore delectus modi nam voluptatibus excepturi reiciendis autem, earum odit consectetur quaerat provident. <br><br> Expedita ex eaque, aperiam delectus dolorem corporis, inventore dicta illum laboriosam voluptates tempora.`,
+		infoHeader: {
+			sub: 'HOW WE WORK',
+			mainColor: 'our',
+			accentColor: 'process'
+		},
+		infoBlurb: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto a sunt ad tenetur incidunt culpa nisi totam, atque mollitia soluta asperiores veniam, reprehenderit iste tempore delectus modi nam voluptatibus excepturi reiciendis autem, earum odit consectetur quaerat provident. <br><br> Expedita ex eaque, aperiam delectus dolorem corporis, inventore dicta illum laboriosam voluptates tempora.`,
+		processList: [
+			{
+				title: 'technical consultation',
+				steps: ['Dolor sit amet', 'Adipiscing elit', 'Mauris eu risus', 'Dapibus neque']
+			},
+			{
+				title: 'patching services',
+				steps: ['Dolor sit amet', 'Adipiscing elit', 'Mauris eu risus', 'Dapibus neque']
+			},
+			{
+				title: 'network setup',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			},
+			{
+				title: 'server racking',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			},
+			{
+				title: 'server blades',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			},
+			{
+				title: 'system maintenance',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			}
+		],
+		contactHeader: {
+			sub: 'CONTACT',
+			mainColor: 'start your <br> fiber optic project',
+			accentColor: 'today'
+		},
+		contactBlurb: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto a sunt ad tenetur incidunt culpa nisi totam, atque mollitia soluta asperiores veniam, reprehenderit iste tempore delectus modi nam voluptatibus excepturi reiciendis autem, earum odit consectetur quaerat provident. <br><br> Expedita ex eaque, aperiam delectus dolorem corporis, inventore dicta illum laboriosam voluptates tempora. Expedita ex eaque, aperiam delectus dolorem corporis, inventore dicta illum laboriosam voluptates tempora.`
+	},
+	'voip': {
+		title: 'Internet & VoIP',
+		heroImg: '/services/voip.jpg',
+		heroHeader: {
+			sub: 'SERVICES',
+			mainColor: 'VoIP & phone',
+			accentColor: 'solutions'
+		},
+		heroBlurb: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto a sunt ad tenetur incidunt culpa nisi totam, atque mollitia soluta asperiores veniam, reprehenderit iste tempore delectus modi nam voluptatibus excepturi reiciendis autem, earum odit consectetur quaerat provident. <br><br> Expedita ex eaque, aperiam delectus dolorem corporis, inventore dicta illum laboriosam voluptates tempora.`,
+		infoHeader: {
+			sub: 'HOW WE WORK',
+			mainColor: 'our',
+			accentColor: 'process'
+		},
+		infoBlurb: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto a sunt ad tenetur incidunt culpa nisi totam, atque mollitia soluta asperiores veniam, reprehenderit iste tempore delectus modi nam voluptatibus excepturi reiciendis autem, earum odit consectetur quaerat provident. <br><br> Expedita ex eaque, aperiam delectus dolorem corporis, inventore dicta illum laboriosam voluptates tempora.`,
+		processList: [
+			{
+				title: 'technical consultation',
+				steps: ['Dolor sit amet', 'Adipiscing elit', 'Mauris eu risus', 'Dapibus neque']
+			},
+			{
+				title: 'patching services',
+				steps: ['Dolor sit amet', 'Adipiscing elit', 'Mauris eu risus', 'Dapibus neque']
+			},
+			{
+				title: 'network setup',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			},
+			{
+				title: 'server racking',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			},
+			{
+				title: 'server blades',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			},
+			{
+				title: 'system maintenance',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			}
+		],
+		contactHeader: {
+			sub: 'CONTACT',
+			mainColor: 'start your <br> VoIP project',
+			accentColor: 'today'
+		},
+		contactBlurb: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto a sunt ad tenetur incidunt culpa nisi totam, atque mollitia soluta asperiores veniam, reprehenderit iste tempore delectus modi nam voluptatibus excepturi reiciendis autem, earum odit consectetur quaerat provident. <br><br> Expedita ex eaque, aperiam delectus dolorem corporis, inventore dicta illum laboriosam voluptates tempora. Expedita ex eaque, aperiam delectus dolorem corporis, inventore dicta illum laboriosam voluptates tempora.`
+	},
+	'security': {
+		title: 'Security Systems',
+		heroImg: '/services/security.jpg',
+		heroHeader: {
+			sub: 'SERVICES',
+			mainColor: 'security',
+			accentColor: 'systems'
+		},
+		heroBlurb: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto a sunt ad tenetur incidunt culpa nisi totam, atque mollitia soluta asperiores veniam, reprehenderit iste tempore delectus modi nam voluptatibus excepturi reiciendis autem, earum odit consectetur quaerat provident. <br><br> Expedita ex eaque, aperiam delectus dolorem corporis, inventore dicta illum laboriosam voluptates tempora.`,
+		infoHeader: {
+			sub: 'HOW WE WORK',
+			mainColor: 'our',
+			accentColor: 'process'
+		},
+		infoBlurb: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto a sunt ad tenetur incidunt culpa nisi totam, atque mollitia soluta asperiores veniam, reprehenderit iste tempore delectus modi nam voluptatibus excepturi reiciendis autem, earum odit consectetur quaerat provident. <br><br> Expedita ex eaque, aperiam delectus dolorem corporis, inventore dicta illum laboriosam voluptates tempora.`,
+		processList: [
+			{
+				title: 'technical consultation',
+				steps: ['Dolor sit amet', 'Adipiscing elit', 'Mauris eu risus', 'Dapibus neque']
+			},
+			{
+				title: 'patching services',
+				steps: ['Dolor sit amet', 'Adipiscing elit', 'Mauris eu risus', 'Dapibus neque']
+			},
+			{
+				title: 'network setup',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			},
+			{
+				title: 'server racking',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			},
+			{
+				title: 'server blades',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			},
+			{
+				title: 'system maintenance',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			}
+		],
+		contactHeader: {
+			sub: 'CONTACT',
+			mainColor: 'start your <br> security project',
+			accentColor: 'today'
+		},
+		contactBlurb: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto a sunt ad tenetur incidunt culpa nisi totam, atque mollitia soluta asperiores veniam, reprehenderit iste tempore delectus modi nam voluptatibus excepturi reiciendis autem, earum odit consectetur quaerat provident. <br><br> Expedita ex eaque, aperiam delectus dolorem corporis, inventore dicta illum laboriosam voluptates tempora. Expedita ex eaque, aperiam delectus dolorem corporis, inventore dicta illum laboriosam voluptates tempora.`
+	},
+	'server-repair': {
+		title: 'Server Repair',
+		heroImg: '/services/repair.jpg',
+		heroHeader: {
+			sub: 'SERVICES',
+			mainColor: 'server repair',
+			accentColor: 'expertise'
+		},
+		heroBlurb: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto a sunt ad tenetur incidunt culpa nisi totam, atque mollitia soluta asperiores veniam, reprehenderit iste tempore delectus modi nam voluptatibus excepturi reiciendis autem, earum odit consectetur quaerat provident. <br><br> Expedita ex eaque, aperiam delectus dolorem corporis, inventore dicta illum laboriosam voluptates tempora.`,
+		infoHeader: {
+			sub: 'HOW WE WORK',
+			mainColor: 'our',
+			accentColor: 'process'
+		},
+		infoBlurb: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto a sunt ad tenetur incidunt culpa nisi totam, atque mollitia soluta asperiores veniam, reprehenderit iste tempore delectus modi nam voluptatibus excepturi reiciendis autem, earum odit consectetur quaerat provident. <br><br> Expedita ex eaque, aperiam delectus dolorem corporis, inventore dicta illum laboriosam voluptates tempora.`,
+		processList: [
+			{
+				title: 'technical consultation',
+				steps: ['Dolor sit amet', 'Adipiscing elit', 'Mauris eu risus', 'Dapibus neque']
+			},
+			{
+				title: 'patching services',
+				steps: ['Dolor sit amet', 'Adipiscing elit', 'Mauris eu risus', 'Dapibus neque']
+			},
+			{
+				title: 'network setup',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			},
+			{
+				title: 'server racking',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			},
+			{
+				title: 'server blades',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			},
+			{
+				title: 'system maintenance',
+				steps: [
+					'Dolor sit amet',
+					'Adipiscing elit',
+					'Mauris eu risus',
+					'Dapibus neque',
+					'Risus id metus',
+					'Cras ornare'
+				]
+			}
+		],
+		contactHeader: {
+			sub: 'CONTACT',
+			mainColor: 'start your <br> repair process',
+			accentColor: 'today'
+		},
+		contactBlurb: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto a sunt ad tenetur incidunt culpa nisi totam, atque mollitia soluta asperiores veniam, reprehenderit iste tempore delectus modi nam voluptatibus excepturi reiciendis autem, earum odit consectetur quaerat provident. <br><br> Expedita ex eaque, aperiam delectus dolorem corporis, inventore dicta illum laboriosam voluptates tempora. Expedita ex eaque, aperiam delectus dolorem corporis, inventore dicta illum laboriosam voluptates tempora.`
+	},
+};
