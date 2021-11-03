a77bb37809b799d93442abf34523ada52fc22e31
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2021-11-03 16:59:43 +0700

    Replace CaseStudies with client testimonials.

---
+++ src/lib/index/testimonial/Tstm.svelte
@@ -0,0 +1,175 @@
+<script>
+	import { goto } from '$app/navigation';
+
+	import QuoteMark from './QuoteMark.svelte';
+	import Arrow from '$lib/Arrow.svelte';
+
+	export let client;
+	export let imgUrlFrag;
+	export let company;
+	export let quote;
+	export let link;
+	export let height = '40vh';
+	export let width = '30vw';
+</script>
+
+<li
+	class="testimonial"
+	style="height: {height}; width: {width}"
+	on:click={() => goto('/case-studies')}
+>
+	<div class="quote">
+		<div class="quote__mark-wrap"><QuoteMark /></div>
+		<p class="quote__text">
+			{quote}
+		</p>
+		<button class="read-more"> ...read more </button>
+	</div>
+	<div class="meta">
+		<figure class="client-img">
+			<img
+				src={`https://res.cloudinary.com/datacom-cabling/image/upload/f_auto,q_50/v1635931310/testimonials/${imgUrlFrag}.jpg`}
+				alt={`${client} from ${company}`}
+			/>
+			<figcaption class="client-info">
+				<span class="name">{client}</span>
+				<span class="company">{company}</span>
+			</figcaption>
+		</figure>
+		<button class="case-link">
+			<span>Read Case Study</span>
+			<span class="arrow-wrap">
+				<Arrow direction="right" />
+			</span>
+		</button>
+	</div>
+</li>
+
+<style lang="scss">
+	.testimonial {
+		-webkit-user-select: none;
+		cursor: pointer;
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		@media (max-width: 1024px) {
+			width: 90vw !important;
+			height: 50vh !important;
+			margin: 0 !important;
+			margin-bottom: 5vw !important;
+		}
+		&:not(:last-child) {
+			margin-right: 100px;
+		}
+		.quote {
+			position: relative;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			&:hover {
+				.read-more {
+					color: var(--primary-tr1);
+					border-bottom: 1px solid var(--primary-tr1);
+				}
+			}
+			&__text {
+				width: 75%;
+				text-align: center;
+				font-size: var(--text-prose);
+				font-style: italic;
+				color: var(--text-color);
+			}
+			&__mark-wrap {
+				position: absolute;
+				top: -20%;
+				left: 10%;
+			}
+			&::after {
+				z-index: var(--base-one);
+				position: absolute;
+				content: '';
+				top: 50%;
+				left: 50%;
+				transform: translate(-50%, -55%);
+				width: 35%;
+				aspect-ratio: 1 / 1;
+				background-color: var(--accent-tr2);
+				border-radius: var(--radius-rounded);
+			}
+			.read-more {
+				cursor: pointer;
+				align-self: flex-end;
+				background-color: transparent;
+				border: none;
+				margin-right: var(--space-5xl);
+				font-size: var(--text-lg);
+				font-style: italic;
+				color: var(--deep-cyan-tr2);
+				padding-bottom: var(--space-md);
+				border-bottom: 1px solid var(--deep-cyan-tr2);
+				transition: var(--transition-3-smooth);
+			}
+		}
+		.meta {
+			display: flex;
+			justify-content: space-between;
+			.client-img {
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				img {
+					height: var(--icon-2xl);
+					width: var(--icon-2xl);
+					border-radius: var(--radius-rounded);
+					border: 3px solid var(--accent-color);
+					object-fit: cover;
+				}
+			}
+			.client-info {
+				margin-left: var(--space-lg);
+				display: flex;
+				flex-direction: column;
+				align-items: flex-start;
+				justify-content: center;
+				font-size: var(--text-2xl);
+				color: var(--text-color);
+				.name {
+					font-weight: var(--light);
+					font-style: italic;
+				}
+				.company {
+					font-weight: var(--bold);
+				}
+			}
+			.case-link {
+				cursor: pointer;
+				background-color: transparent;
+				border: none;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				color: var(--text-color);
+				font-size: var(--text-lg);
+				font-weight: var(--semibold);
+				&:hover {
+					.arrow-wrap {
+						transform: translateX(10%) scaleX(1.05);
+					}
+				}
+				.arrow-wrap {
+					margin-left: var(--space-md);
+					display: flex;
+					align-items: center;
+					height: var(--icon-sm);
+					width: var(--icon-sm);
+					transition: var(--transition-3-smooth);
+					svg {
+						height: 25px;
+						width: 25px;
+					}
+				}
+			}
+		}
+	}
+</style>
---
+++ src/lib/index/testimonial/TstmSection.svelte
@@ -0,0 +1,201 @@
+<script>
+	import Testimonial from './Tstm.svelte';
+	import Container from '$lib/containers/Container.svelte';
+	import SectionHeader from '$lib/SectionHeader.svelte';
+	import Arrow from '$lib/Arrow.svelte';
+	import BGCircle from '$lib/background/BGCircle.svelte';
+
+	let testimonialData = [
+		{
+			quote:
+				'Our business took off far more quickly than we anticipated, and our hardware simply couldn’t keep up. Datacom got us back on track quickly, and without interruption. Excellent work.',
+			client: 'Abdullah Slimani',
+			imgUrlFrag: 'abdullah_xdqqw2',
+			company: 'imgIX Data Center',
+			caseLink: '/'
+		},
+		{
+			quote:
+				'Even with our extremely close deadline , Datacom was able to help us choose the right security system and get it installed ahead of our launch, and inside our budget! Highly recommened. ',
+			client: 'Chris McGeorge',
+			imgUrlFrag: 'chris_fjcgld',
+			company: 'YSA Athletics',
+			caseLink: '/'
+		},
+		{
+			quote:
+				'With online learning becoming more important in higher education, we needed a network refresh more than ever. Datacom assessed our issues, and was quickly able to execute. We couldn’t be happier.',
+			client: 'Eleanor Nakahara',
+			imgUrlFrag: 'eleanor_n2tmgn',
+			company: 'Hayward City College',
+			caseLink: '/'
+		},
+		{
+			quote:
+				'With online learning becoming more important in higher education, we needed a network refresh more than ever. Datacom assessed our issues, and was quickly able to execute. We couldn’t be happier.',
+			client: 'Besty Graham',
+			imgUrlFrag: 'betsy_c9aybh',
+			company: 'Hayward City College',
+			caseLink: '/'
+		},
+		{
+			quote:
+				'With online learning becoming more important in higher education, we needed a network refresh more than ever. Datacom assessed our issues, and was quickly able to execute. We couldn’t be happier.',
+			client: 'Patrice Beaumont',
+			imgUrlFrag: 'patrice_zwdnin',
+			company: 'Hayward City College',
+			caseLink: '/'
+		},
+	];
+
+	let scrollIndex = 0;
+	let scrollPosition = 0;
+	let casesScrolled;
+
+	const scrollCases = (direction) => {
+		const scrollDistance = 35;
+
+		// prevent scroll if already translated
+		if (casesScrolled) return;
+
+		if (direction === 'right' && scrollIndex <= 2) {
+			scrollIndex++;
+		} else if (direction === 'left' && scrollIndex > 0) {
+			scrollIndex--;
+		}
+		scrollPosition = scrollDistance * scrollIndex;
+	};
+</script>
+
+<section class="testimonials" id="testimonials">
+	<div class="bgcircle-wrap">
+		<BGCircle />
+	</div>
+	<Container column>
+		<div class="upper">
+			<div class="upper-left">
+				<SectionHeader
+					sub={'HOW WE WORK'}
+					titleMainColor={'customer'}
+					titleYellow={'testimonials'}
+				/>
+				<div class="section-blurb">
+					We strive to build strong relationships with our customers. Read their thoughts on our
+					work.
+				</div>
+			</div>
+			<div class="arrows">
+				<span on:click={() => scrollCases('left')}
+					><Arrow filled={scrollIndex !== 0} direction="left" /></span
+				>
+				<span on:click={() => scrollCases('right')}
+					><Arrow filled={scrollIndex !== 3} direction="right" /></span
+				>
+			</div>
+		</div>
+		<div
+			class="lower"
+			on:scroll={(e) => {
+				if (scrollIndex > 0) {
+					e.target.scrollLeft = 0;
+				}
+				casesScrolled = e.target.scrollLeft > 0;
+			}}
+		>
+			<ul class="testimonials__list" style={`transform: translate(-${scrollPosition}vw)`}>
+				{#each testimonialData as { client, company, quote, imgUrlFrag, caseLink: link } (client)}
+					<Testimonial {client} {imgUrlFrag} {company} {quote} {link} />
+				{/each}
+			</ul>
+		</div>
+	</Container>
+</section>
+
+<style lang="scss">
+	.bgcircle-wrap {
+		z-index: var(--ground);
+		position: absolute;
+		top: 10%;
+		left: -25%;
+	}
+	.testimonials {
+		position: relative;
+		height: 140vh;
+		width: 100vw;
+		background: var(--white);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		color: var(--white);
+		overflow: hidden;
+		&__list {
+			min-width: 180vw;
+			height: 50vh;
+			margin: 0;
+			padding: 0;
+			padding-left: 20vw;
+			display: flex;
+			align-items: center;
+			justify-content: flex-start;
+			overflow: visible;
+			transition: var(--transition-1-smooth);
+			will-change: transform;
+			@media (max-width: 1024px) {
+				height: max-content;
+				min-width: 100%;
+				padding: 0;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+			}
+		}
+		@media (max-width: 1024px) {
+			height: max-content;
+			padding-top: var(--mobile-sec-margin);
+		}
+	}
+	.upper {
+		z-index: var(--level-top);
+		width: 100%;
+		margin-bottom: var(--space-5xl);
+		display: flex;
+		justify-content: space-between;
+		align-items: flex-end;
+		.section-blurb {
+			width: 55%;
+			margin-top: var(--title-margin);
+			font-size: var(--text-lg);
+			color: var(--text-color);
+			@media (max-width: 1024px) {
+				width: 90vw;
+				margin-left: 5vw;
+			}
+		}
+		.arrows {
+			width: 200px;
+			width: var(--vp-2xl);
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			@media (max-width: 1024px) {
+				display: none;
+			}
+		}
+	}
+	.lower {
+		width: 100vw;
+		transform: translateX(-20vw);
+		height: max-content;
+		overflow-x: scroll;
+		display: flex;
+		align-items: flex-start;
+		justify-content: flex-start;
+		&::-webkit-scrollbar {
+			display: none;
+		}
+		@media (max-width: 1024px) {
+			transform: unset;
+		}
+	}
+</style>
+
